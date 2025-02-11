3991e531677ee9c6d3f780b0de3d44acda57f105
Pedro Gimenez <gimenezcavalcantepedro@gmail.com>
2025-02-11 11:52:09 -0300

    👌 MELHORIA: finish orgs routes

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -22,6 +22,9 @@ import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizatios'
+import { shutDownOrganization } from './routes/orgs/shutdown-organization'
+import { transferOrganization } from './routes/orgs/transfer-organization'
+import { updateOrganization } from './routes/orgs/update-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -71,6 +74,9 @@ app.register(createOrganization)
 app.register(getMembership)
 app.register(getOrganization)
 app.register(getOrganizations)
+app.register(updateOrganization)
+app.register(shutDownOrganization)
+app.register(transferOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`HTTP server is running`)
