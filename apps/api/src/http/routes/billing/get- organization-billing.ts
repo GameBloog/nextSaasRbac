15c57795c484174e115cb790b7a690123d855f8c
Pedro Gimenez <gimenezcavalcantepedro@gmail.com>
2025-02-13 15:15:40 -0300

    👌 MELHORIA: get organization billing

---
+++ apps/api/src/http/routes/billing/get- organization-billing.ts
@@ -0,0 +1,90 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-users-permissions'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function getOrganizationBiiling(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/billing',
+      {
+        schema: {
+          tags: ['billing'],
+          summary: 'Get billing information from organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              billing: z.object({
+                seats: z.object({
+                  amount: z.number(),
+                  unit: z.number(),
+                  price: z.number(),
+                }),
+                projects: z.object({
+                  amount: z.number(),
+                  unit: z.number(),
+                  price: z.number(),
+                }),
+                total: z.number(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('get', 'Billing')) {
+          throw new BadRequestError(
+            `You're not allowed to get billing details from this organization`,
+          )
+        }
+
+        const [amaountOfMembers, amountOfProject] = await Promise.all([
+          prisma.member.count({
+            where: {
+              organizationId: organization.id,
+              role: { not: 'BILLING' },
+            },
+          }),
+
+          prisma.project.count({
+            where: {
+              organizationId: organization.id,
+            },
+          }),
+        ])
+
+        return {
+          billing: {
+            seats: {
+              amount: amaountOfMembers,
+              unit: 10,
+              price: amaountOfMembers * 10,
+            },
+            projects: {
+              amount: amountOfProject,
+              unit: 20,
+              price: amountOfProject * 20,
+            },
+            total: amaountOfMembers * 10 + amountOfProject * 20,
+          },
+        }
+      },
+    )
+}
